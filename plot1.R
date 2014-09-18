b2eeca18c94a5423f6c4b48e8fcc759cc2aa2f1b
rmenon <rmenon.us@gmail.com>
2014-09-17 21:37:12 -0700

    first plot

---
+++ plot1.R
@@ -0,0 +1,8 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI_yearly <- NEI %>%
+  group_by(year) %>%
+    summarize(yearlyEmissions=sum(Emissions, na.rm=TRUE))
+png("plot1.png", width=480, height=480)
+plot(NEI_yearly$year,NEI_yearly$yearlyEmissions,type='l', xlab="Year", ylab="Total Emissions", main=expression("Total " ~ PM[2.5] ~ " Emissions in US"))
+dev.off()
