ae5c8cd51b5d1850aa2e589e3038ed828e927490
rmenon <rmenon.us@gmail.com>
2014-09-18 10:34:02 -0700

    plot3 refactor; plot4

--- plot3.R
+++ plot3.R
@@ -4,7 +4,6 @@ SCC <- readRDS("Source_Classification_Code.rds")
 NEI_yearly <- NEI %>%
   filter(fips == "24510") %>%
   group_by(year, type) %>%
-    summarize(yearlyEmissions=sum(Emissions, na.rm=TRUE))
-png("plot3.png", width=480, height=480);
-qplot(year, yearlyEmissions, data=NEI_yearly, color=type, facets=.~type, geom=c("point", "smooth"));
-dev.off();
+    summarize(emissions=sum(Emissions, na.rm=TRUE))
+plot <- qplot(year, emissions, data=NEI_yearly, color=type, facets=.~type, geom=c("point", "smooth"), main=expression("Total " ~ PM[2.5] ~ " Emissions in Baltimore, Maryland, USA"));
+ggsave (filename="plot3.png", plot=plot, height=3, width=10, dpi=100)
---
+++ plot4.R
@@ -0,0 +1,11 @@
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+NEI_yearly <- 
+  SCC %>% 
+    filter(grepl("Coal", EI.Sector)) %>% 
+      inner_join(NEI, by=c("SCC")) %>% 
+        group_by(year) %>% 
+          summarize(emissions = sum(Emissions, na.rm=TRUE))
+
+p <- qplot(year, emissions, data=NEI_yearly,geom=c("point", "smooth"), main=expression("Total Coal related " ~ PM[2.5] ~ " Emissions in US"))
+ggsave(filename="plot4.png", p, width=5, height=5, dpi=100)
