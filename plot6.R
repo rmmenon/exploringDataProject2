133283a55458edc77e40637c0216795caca09160
rmenon <rmenon.us@gmail.com>
2014-09-18 19:28:00 -0700

    plot 6

--- plot6.R
+++ plot6.R
@@ -8,5 +8,5 @@ onRoadSummary <-
       group_by(year, fips) %>% 
         summarize(emissions = sum(Emissions, na.rm=TRUE))
 
-p <- qplot(year, emissions, data=onRoadSummary,geom=c("point", "smooth"), facets=.~fips, main=expression("Total Motor Vehicles related " ~ PM[2.5] ~ " Emissions Baltimore Vs Los Angeles, US"))
-ggsave(filename="plot6.png", p, width=10, height=5, dpi=100)
+p <- qplot(year, emissions, data=onRoadSummary,geom=c("point", "smooth"), facets=.~fips, main=expression("Motor Vehicles related " ~ PM[2.5] ~ " Emissions Baltimore Vs Los Angeles"))
+ggsave(filename="plot6.png", p, width=7, height=5, dpi=100)
